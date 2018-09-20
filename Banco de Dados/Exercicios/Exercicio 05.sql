a29697fe47e35a72abbf0198aa5b5189a04ea1c3
josielalv16 <josielalv@hotmail.com>
2018-09-20 09:16:07 -0300

    Exercicios

--- Banco de Dados/Exercicios/Exercicio 05.sql
+++ Banco de Dados/Exercicios/Exercicio 05.sql
@@ -1,17 +1,12 @@
-SELECT C.NomeCliente, COUNT(VI.IdProduto) AS Quantidade 
-	FROM VendaItem AS VI
-	INNER JOIN Venda AS V
-	ON VI.IdVenda = V.IdVenda
-	INNER JOIN Cliente AS C
-	ON V.IdCliente = C.IdCliente
-	WHERE DATEPART(YY, V.DataVenda) = 2018
-	GROUP BY C.NomeCliente, VI.IdProduto
-	
-	
-SELECT * FROM Cliente WHERE NomeCliente = 'Josiel Alves de Matos'
-SELECT * FROM Venda WHERE IdCliente = 27
-SELECT * FROM VendaItem WHERE IdVenda = 77 or IdVenda = 79
-
-INSERT INTO Venda VALUES (27, '21/05/2017 13:15:20', '22/05/2017 13:15:20')
-INSERT INTO VendaItem VALUES(79, 2, 3)
-INSERT INTO VendaItem VALUES(78, 3, 3)
+SELECT Resultado.Nome, COUNT(Resultado.idProduto)
+	FROM
+	(SELECT C.NomeCliente AS Nome, VI.IdProduto AS idProduto
+		FROM VendaItem AS VI
+		INNER JOIN Venda AS V
+		ON VI.IdVenda = V.IdVenda
+		INNER JOIN Cliente AS C
+		ON V.IdCliente = C.IdCliente
+		WHERE DATEPART(YY, V.DataVenda) = 2018
+		GROUP BY C.NomeCliente, VI.IdProduto) as Resultado
+	GROUP BY Resultado.Nome
+	ORDER BY Resultado.Nome
---
+++ Banco de Dados/Exercicios/Exercicio 08.sql
@@ -0,0 +1,9 @@
+SELECT TOP 10 C.NomeCliente, C.Sexo, SUM(VI.Quantidade) as Quantidade, V.DataVenda
+	FROM VendaItem AS VI
+	INNER JOIN Venda AS V
+	ON VI.IdVenda = V.IdVenda
+	INNER JOIN Cliente AS C
+	ON V.IdCliente = C.IdCliente
+	WHERE V.DataPagamento IS NULL
+	GROUP BY C.NomeCliente, C.Sexo, V.DataVenda
+	ORDER BY V.DataVenda
