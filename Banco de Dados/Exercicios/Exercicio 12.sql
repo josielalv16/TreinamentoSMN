50987de86cb569e07599bcbffd1c007d02e492eb
josielalv16 <josielalv@hotmail.com>
2018-09-20 16:16:18 -0300

    Exercicios

--- Banco de Dados/Exercicios/Exercicio 12.sql
+++ Banco de Dados/Exercicios/Exercicio 12.sql
@@ -1,9 +1,12 @@
-SELECT top 10 P.NomeProduto, M.NomeMarca, SUM(VI.Quantidade) AS Quantidade, P.ValorVenda, P.ValorCompra,
-       (P.ValorVenda-P.ValorCompra)*(VI.Quantidade) as Lucro
-FROM Produto AS P
-INNER JOIN Marca AS M ON M.IdMarca = P.IdMarca
-INNER JOIN VendaItem AS VI ON VI.IdProduto = P.IdProduto
-INNER JOIN Venda AS V ON V.IdVenda = VI.IdVenda
-WHERE V.DataPagamento is not null and V.DataPagamento < GETDATE()
-GROUP BY P.NomeProduto, M.NomeMarca, P.ValorVenda, P.ValorCompra,VI.Quantidade
-ORDER BY Lucro desc
+SELECT TOP 10 P.NomeProduto, M.NomeMarca, SUM(VI.Quantidade) AS Quantidade, 
+			(P.ValorVenda - P.ValorCompra) * (VI.Quantidade) AS Lucro
+	FROM Produto AS P
+	INNER JOIN Marca AS M 
+	ON M.IdMarca = P.IdMarca
+	INNER JOIN VendaItem AS VI 
+	ON VI.IdProduto = P.IdProduto
+	INNER JOIN Venda AS V 
+	ON V.IdVenda = VI.IdVenda
+	WHERE V.DataPagamento IS NOT NULL AND V.DataPagamento < GETDATE()
+	GROUP BY P.NomeProduto, M.NomeMarca, P.ValorVenda, P.ValorCompra,VI.Quantidade
+	ORDER BY Lucro desc
